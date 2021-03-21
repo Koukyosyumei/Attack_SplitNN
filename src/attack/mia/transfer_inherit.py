822ff615e42eaf815951e322e0db1fa7f989f29d
Syumei <koukyosyumei@hotmail.com>
2021-03-21 22:45:24 +0900

    add methods

--- src/attack/mia/transfer_inherit.py
+++ src/attack/mia/transfer_inherit.py
@@ -2,18 +2,28 @@ import torch
 
 
 class mia_transfer_inherit:
-    def __init__(self, shadow_client, server, device="cpu"):
+    def __init__(self, shadow_client,
+                 server,
+                 device="cpu"):
 
         self.shadow_client = shadow_client
         self.server = server
         self.device = device
 
-    def fit(self, shadowloader, epochs, metric=None):
+        self.attacker_X_train_shadow = None
+        self.attacker_X_test_shadow = None
+        self.attacker_y_train_shadow = None
+        self.attacker_y_test_shadow = None
+
+    def fit(self, member_shadowloader, nonmember_shadowloader):
+        pass
+
+    def _fit_shadow_model(self, member_shadowloader, epochs, metric=None):
         for epoch in range(epochs):
             epoch_loss = 0
             epoch_labels = []
             epoch_outputs = []
-            for i, data in enumerate(shadowloader, 0):
+            for i, data in enumerate(member_shadowloader, 0):
 
                 self.shadow_client.client_optimizer.zero_grad()
 
@@ -32,7 +42,7 @@ class mia_transfer_inherit:
 
                 self.shadow_client.client_optimizer.step()
 
-                epoch_loss += loss / len(shadowloader.dataset)
+                epoch_loss += loss / len(member_shadowloader.dataset)
                 epoch_outputs.append(outputs)
                 epoch_labels.append(labels)
 
@@ -43,8 +53,35 @@ class mia_transfer_inherit:
                                epoch_outputs, epoch_labels,
                                metric=metric)
 
-    def predict(self):
-        pass
+    def _create_dataset_for_attacker(self, member_shadowloader,
+                                     nonmember_shadowloader):
+        self.attacker_X_train_shadow = self._predict_shadow_model(
+            member_shadowloader)
+        self.attacker_X_test_shadow = self._predict_shadow_model(
+            nonmember_shadowloader)
+        self.attacker_y_train_shadow = torch.ones(
+            self.attacker_X_train_shadow.shape[0])
+        self.attacker_y_test_shadow = torch.zeros(
+            self.attacker_X_test_shadow.shape[0])
+
+    def _predict_shadow_model(self, dataloader):
+        with torch.no_grad():
+            train_shadow = []
+            for i, data in enumerate(dataloader, 0):
+                inputs, labels = data
+                inputs = inputs.to(self.device)
+                labels = labels.to(self.device)
+
+                # execute client - feed forward network
+                intermediate = self.shadow_client(inputs)
+                remote_intermidiate = intermediate.detach().requires_grad_()
+                # execute server - feed forward network
+                output = self.server(remote_intermidiate)
+
+                train_shadow.append(output)
+
+        outputs = torch.cat(train_shadow)
+        return outputs
 
     def _print_metric(self, epoch,
                       epoch_loss,
