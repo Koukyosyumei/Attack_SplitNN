440c3a2cbbb9b50a044a6387547245096e680007
Syumei <koukyosyumei@hotmail.com>
2021-03-25 22:13:36 +0900

    fix init

--- src/attacksplitnn/attack/__init__.py
+++ src/attacksplitnn/attack/__init__.py
@@ -1,4 +1,4 @@
 from .labelleakage.normattack import NormAttack
-from .mia.splitmia import SplitMIA
+from .membershipinference.transferinherit import TransferInherit
 from .modelinversion import fsha, fshamnist
 from .modelinversion.blackbox import Black_Box_Model_Inversion
